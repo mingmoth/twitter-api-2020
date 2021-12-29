5d05aa1e659bf3602af20080aa5146383ffe1c67
mingmoth <mingmoth@gmail.com>
2021-12-29 21:58:10 +0800

    recover original models/user.js

--- models/user.js
+++ models/user.js
@@ -3,23 +3,35 @@ const {
   Model
 } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    static associate(models) {
-      // define association here
-    }
-  };
-  User.init({
-    name: DataTypes.STRING,
+  const User = sequelize.define('User', {
+    name: DataTypes.STRING(50),
     email: DataTypes.STRING,
     account: DataTypes.STRING,
     password: DataTypes.STRING,
     avatar: DataTypes.STRING,
     cover: DataTypes.STRING,
-    introduction: DataTypes.TEXT,
+    introduction: DataTypes.TEXT(160),
     role: DataTypes.STRING,
-  }, {
-    sequelize,
-    modelName: 'User'
-  })
+  }, {});
+  User.associate = function (models) {
+  };
+  // class User extends Model {
+  //   static associate(models) {
+  //     // define association here
+  //   }
+  // };
+  // User.init({
+  //   name: DataTypes.STRING,
+  //   email: DataTypes.STRING,
+  //   account: DataTypes.STRING,
+  //   password: DataTypes.STRING,
+  //   avatar: DataTypes.STRING,
+  //   cover: DataTypes.STRING,
+  //   introduction: DataTypes.TEXT,
+  //   role: DataTypes.STRING,
+  // }, {
+  //   sequelize,
+  //   modelName: 'User'
+  // })
   return User
 }
