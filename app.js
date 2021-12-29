63f38e4a82d622b9c5b956d329c7f358b5b40702
mingmoth <mingmoth@gmail.com>
2021-12-29 14:47:52 +0800

    install bcryptjs & use body-parser & session/flash in app.js & add userController

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const helpers = require('./_helpers');
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const flash = require('connect-flash')
 
 const app = express()
 const port = 3000
 
+// use body-parser
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+// use session and flash
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+
 // use helpers.getUser(req) to replace req.user
-function authenticated(req, res, next){
+function authenticated(req, res, next) {
   // passport.authenticate('jwt', { ses...
 };
 
---
+++ controllers/api/userController.js
@@ -0,0 +1,7 @@
+const bcrypt = require('bcryptjs')
+const db = require('../../models')
+const User = db.User
+
+const userController = {}
+
+module.exports = userController
