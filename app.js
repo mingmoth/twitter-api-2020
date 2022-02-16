6ec194194b0d632f5bf6ebad924a1a0c1050af09
mingmoth <mingmoth@gmail.com>
2022-02-16 17:18:59 +0800

    modify: delete redundant code

--- app.js
+++ app.js
@@ -2,14 +2,10 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 const express = require('express')
-const helpers = require('./_helpers');
 const bodyParser = require('body-parser')
-// const session = require('express-session')
-const passport = require('./config/passport')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
-
 
 const { createServer } = require('http')
 const { Server } = require('socket.io')
@@ -19,29 +15,11 @@ const httpServer = createServer(app)
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
-// use session and flash
-// app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
-
-
-
-// setup passport
-app.use(passport.initialize())
-app.use(passport.session())
-
-// use helpers.getUser(req) to replace req.user
-app.use((req, res, next) => {
-  res.locals.user = helpers.getUser(req)
-  next()
-})
-
 // cors setting
 app.use(cors())
 
 // socket.io setting
 require('./socket/socket')(Server, httpServer)
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
 
 // route app using ./routes
 require('./routes')(app)
--- config/passport.js
+++ config/passport.js
@@ -1,46 +1,9 @@
 const passport = require('passport')
-const LocalStrategy = require('passport-local')
-const bcrypt = require('bcryptjs')
 
 require('dotenv').config()
 
 const db = require('../models')
 const User = db.User
-
-passport.use(new LocalStrategy(
-  {
-    usernameField: 'account',
-    passwordField: 'password',
-    passReqToCallback: true
-  },
-  // authenticate user
-  (req, username, password, callback) => {
-    User.findOne({ where: { account: username } }).then(user => {
-      if(!user) {
-        return callback(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤'))
-      }
-      if (!bcrypt.compareSync(password, user.password)) {
-        return callback(null, false, req.flash('error_messages', '密碼輸入錯誤'))
-      }
-      return callback(null, user)
-    })
-  }
-))
-
-passport.serializeUser((user, callback) => {
-  callback(null, user.id)
-})
-passport.deserializeUser((id, callback) => {
-  User.findByPk(id, {
-    include: [
-      { model: User, as: 'Followers' },
-      { model: User, as: 'Followings' }
-    ]
-  }).then(user => {
-    user = user.toJSON() 
-    return callback(null, user)
-  })
-})
 
 // JWT
 const jwt = require('jsonwebtoken')
@@ -53,12 +16,15 @@ jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
 jwtOptions.secretOrKey = process.env.JWT_SECRET
 
 let strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
-  User.findByPk(jwt_payload.id, 
+  User.findByPk(jwt_payload.id,
     {
-    include: [
-      { model: User, as: 'Followers' },
-      { model: User, as: 'Followings' }
-      ]
+      include: [
+        { model: User, as: 'Followers' },
+        { model: User, as: 'Followings' }
+      ],
+      attributes: {
+        exclude: ['password', 'createdAt', 'updatedAt']
+      }
     }
   ).then(user => {
     if (!user) return next(null, false)
